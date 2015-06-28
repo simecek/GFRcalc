a430246dfb1e33b99694d6a1270661c3de33f4bf
simecek <lamparna@gmail.com>
2015-06-27 23:31:12 -0400

    first, very rough version of a model with 2 exponentials

--- plot_GFR.r
+++ plot_GFR.r
@@ -1,23 +1,61 @@
 library(ggplot2)
+library(MASS)
+library(cowplot)
 
 dt <- read.csv("GFR.csv", as.is=TRUE)
 
 animals <- unique(dt$Animal)
-frange <- range(c(dt$M1, dt$M2, dt$M3), na.rm=TRUE)
 
-pdf("plot_GFR.pdf")
+pdf("plot_10examples_GFR.pdf",width=12)
 for (a in animals) {
   tmp <- subset(dt, Animal == a)
-  tmp2 <- data.frame(Time = rep(tmp$Time,3),
-                     Line = rep(c("a","b","c"), each=nrow(tmp)),
-                     Fluorescence = c(tmp$M1, tmp$M2, tmp$M3))
   
-  p <- qplot(y=Fluorescence, x=Time, data=tmp2) +
-    geom_smooth(method = "lm", color=I("black"), linetype=I(2))  +
+  # fit robust linear model
+  fit <- rlm(log(Mmean)~Time, data=tmp)  
+  a1 <- tmp$Mmean[1]/2
+  k1 <- - coef(fit)[2]
+  
+  # fit a model ~ A*exp(-B*Time)
+  oneexp <- nls(Mmean ~ A*exp(-B*Time),
+                   data = tmp[-1,], start= list(A=a1,B=k1))
+  
+  # plot prediction
+  tmp2 <- data.frame(Time = rep(tmp$Time[-1],4),
+                     Line = rep(c("a","b","c","model"), each=nrow(tmp)-1),
+                     Fluorescence = c(tmp$M1[-1], tmp$M2[-1], tmp$M3[-1],
+                                      predict(oneexp)))
+  
+  p1 <- qplot(y=Fluorescence, x=Time, data=tmp2)   +
     geom_line(aes(group=Line, color=Line)) +
-    scale_y_log10() + 
-    ggtitle(a) + 
-    theme_bw()+ theme(legend.position="none") 
-  print(p)
+    ggtitle(paste(a,"one exp model")) + 
+    theme_bw()
+  
+  
+  a1 <- coef(oneexp)[1]
+  k1 <- coef(oneexp)[2]
+  a2 <- a1/10
+  k2 <- k1/10
+  
+  twoexp <- tryCatch(nls(Mmean ~ A*exp(-B*Time) + C*exp(-D*Time),
+                   data = tmp[-1,], start=list(A=a1,B=k1,C=a2, D=k2),
+                   algorithm="port"),
+                   error = function(e) NULL)
+  
+  if (is.null(twoexp)) {
+    pred = rep(mean(tmp$Mmean[-1]),nrow(tmp)-1)
+  } else {
+    pred= predict(twoexp)
+  }  
+  tmp2 <- data.frame(Time = rep(tmp$Time[-1],4),
+                     Line = rep(c("a","b","c","pred"), each=nrow(tmp)-1),
+                     Fluorescence = c(tmp$M1[-1], tmp$M2[-1], tmp$M3[-1],
+                                      pred))
+  
+  p2 <- qplot(y=Fluorescence, x=Time, data=tmp2)   +
+    geom_line(aes(group=Line, color=Line)) +
+    ggtitle(paste(a,"two exp model")) + 
+    theme_bw()
+  print(plot_grid(p1, p2))
 }
 dev.off()
+
