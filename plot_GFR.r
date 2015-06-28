8ee46de7b78c077fe140faee7516cdbe8962f515
simecek <lamparna@gmail.com>
2015-06-28 03:04:13 -0400

    code separated into functions

--- plot_GFR.r
+++ plot_GFR.r
@@ -1,61 +1,75 @@
-library(ggplot2)
-library(MASS)
-library(cowplot)
 
-dt <- read.csv("GFR.csv", as.is=TRUE)
+# check file format
+check.format <- function(tb, col.names=c("M1","M2","M3","Time"), min.rows=6) {
+  if (is.null(tb)) return("")
+  if (!is.data.frame(tb)) return("Not a data frame.")
+  if (!all(col.names %in% names(tb))) return("M1, M2, M3 and Time columns are required.") 
+  for (c in col.names)
+    if (!is.numeric(tb[,"c"])) return(paste("Column",c,"must be numeric."))
+  if (any(is.na(tb$Time))) return("Time must not contain missing values.") 
+  if (nrow(tb) < min.rows) return(paste("Ar least",min.rows,"rows required."))
+  return("")
+}
 
-animals <- unique(dt$Animal)
+# fit log-normal regression
+fit.rlm <- function(y,x) {
+  require(MASS)
+  fit <- rlm(log(y)~x) 
+  fit
+}
 
-pdf("plot_10examples_GFR.pdf",width=12)
-for (a in animals) {
-  tmp <- subset(dt, Animal == a)
+# fit one expondential y = A*exp(-B*x) + noise
+oneexp <- function(y,x) {
+  a1 = exp(coef(fit.rlm(y,x))[1])
+  k1 = -coef(fit.rlm(y,x))[2]
   
-  # fit robust linear model
-  fit <- rlm(log(Mmean)~Time, data=tmp)  
-  a1 <- tmp$Mmean[1]/2
-  k1 <- - coef(fit)[2]
+  tryCatch(nls(y ~ A*exp(-B*x), start= list(A=a1,B=k1)),
+           error = function(e) NULL)
+}
+
+# fit two expondentials y = A*exp(-B*x) + C*exp(-D*x) + noise
+twoexp <- function(y,x) {
+  f1 <- oneexp(y,x)
+  if (is.null(f1)) return(NULL)
   
-  # fit a model ~ A*exp(-B*Time)
-  oneexp <- nls(Mmean ~ A*exp(-B*Time),
-                   data = tmp[-1,], start= list(A=a1,B=k1))
-  
-  # plot prediction
-  tmp2 <- data.frame(Time = rep(tmp$Time[-1],4),
-                     Line = rep(c("a","b","c","model"), each=nrow(tmp)-1),
-                     Fluorescence = c(tmp$M1[-1], tmp$M2[-1], tmp$M3[-1],
-                                      predict(oneexp)))
-  
-  p1 <- qplot(y=Fluorescence, x=Time, data=tmp2)   +
-    geom_line(aes(group=Line, color=Line)) +
-    ggtitle(paste(a,"one exp model")) + 
-    theme_bw()
-  
-  
-  a1 <- coef(oneexp)[1]
-  k1 <- coef(oneexp)[2]
+  a1 <- coef(f1)[1]
+  k1 <- coef(f1)[2]
   a2 <- a1/10
   k2 <- k1/10
   
-  twoexp <- tryCatch(nls(Mmean ~ A*exp(-B*Time) + C*exp(-D*Time),
-                   data = tmp[-1,], start=list(A=a1,B=k1,C=a2, D=k2),
-                   algorithm="port"),
-                   error = function(e) NULL)
-  
-  if (is.null(twoexp)) {
-    pred = rep(mean(tmp$Mmean[-1]),nrow(tmp)-1)
-  } else {
-    pred= predict(twoexp)
-  }  
-  tmp2 <- data.frame(Time = rep(tmp$Time[-1],4),
-                     Line = rep(c("a","b","c","pred"), each=nrow(tmp)-1),
-                     Fluorescence = c(tmp$M1[-1], tmp$M2[-1], tmp$M3[-1],
+  tryCatch(nls(y ~ A*exp(-B*x) + C*exp(-D*x),
+               start=list(A=a1,B=k1,C=a2, D=k2),
+               algorithm="port"),
+           error = function(e) NULL)
+}  
+
+# plot prediction
+plot.pred <- function(dt, pred) {
+  require(ggplot2)
+  tmp2 <- data.frame(Time = rep(dt$Time,4),
+                     Line = rep(c("a","b","c","model"), each=nrow(dt)),
+                     Fluorescence = c(dt$M1, dt$M2, dt$M3,
                                       pred))
   
-  p2 <- qplot(y=Fluorescence, x=Time, data=tmp2)   +
+  qplot(y=Fluorescence, x=Time, data=tmp2) +
     geom_line(aes(group=Line, color=Line)) +
-    ggtitle(paste(a,"two exp model")) + 
     theme_bw()
-  print(plot_grid(p1, p2))
 }
-dev.off()
 
+# analyze one animal
+dt <- read.csv("GFR.csv", as.is=TRUE)
+animals <- unique(dt$Animal)
+a = animals[8]
+tmp <- subset(dt,Animal==a)
+tmp <- tmp[order(tmp$Time),]
+tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
+start <- which.max(tmp$mean)[1]
+tmp <- tmp[start:nrow(tmp),]
+
+fit <- NULL
+while (is.null(fit)) {
+  fit <- twoexp(y=tmp$mean,x=tmp$Time)
+  if (is.null(fit)) {} # filter outliers
+}
+
+plot.pred(tmp, predict(fit)) + ggtitle(paste(a,"two exp model"))
