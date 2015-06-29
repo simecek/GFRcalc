f1f063ffc3d5e97ec0f82f14efad0329956e93de
simecek <lamparna@gmail.com>
2015-06-29 13:23:32 +0200

    algorithm without outlier detection

--- plot_GFR.r
+++ plot_GFR.r
@@ -1,14 +1,15 @@
 
 # check file format
 check.format <- function(tb, col.names=c("M1","M2","M3","Time"), min.rows=6) {
-  if (is.null(tb)) return("")
+  if (is.null(tb)) return(invisible(""))
   if (!is.data.frame(tb)) return("Not a data frame.")
   if (!all(col.names %in% names(tb))) return("M1, M2, M3 and Time columns are required.") 
   for (c in col.names)
-    if (!is.numeric(tb[,"c"])) return(paste("Column",c,"must be numeric."))
+    if (!is.numeric(tb[,c])) return(paste("Column",c,"must be numeric."))
   if (any(is.na(tb$Time))) return("Time must not contain missing values.") 
+  if (any(duplicated(tb$Time))) return("Time must not contain duplicated values.")
   if (nrow(tb) < min.rows) return(paste("Ar least",min.rows,"rows required."))
-  return("")
+  return(invisible(""))
 }
 
 # fit log-normal regression
@@ -18,7 +19,7 @@ fit.rlm <- function(y,x) {
   fit
 }
 
-# fit one expondential y = A*exp(-B*x) + noise
+# fit one component model y = A*exp(-B*x) + noise
 oneexp <- function(y,x) {
   a1 = exp(coef(fit.rlm(y,x))[1])
   k1 = -coef(fit.rlm(y,x))[2]
@@ -27,7 +28,7 @@ oneexp <- function(y,x) {
            error = function(e) NULL)
 }
 
-# fit two expondentials y = A*exp(-B*x) + C*exp(-D*x) + noise
+# fit two components model y = A*exp(-B*x) + C*exp(-D*x) + noise
 twoexp <- function(y,x) {
   f1 <- oneexp(y,x)
   if (is.null(f1)) return(NULL)
@@ -43,11 +44,11 @@ twoexp <- function(y,x) {
            error = function(e) NULL)
 }  
 
-# plot prediction
+# ggplot prediction
 plot.pred <- function(dt, pred) {
   require(ggplot2)
   tmp2 <- data.frame(Time = rep(dt$Time,4),
-                     Line = rep(c("a","b","c","model"), each=nrow(dt)),
+                     Line = rep(c("M1","M2","M3","model"), each=nrow(dt)),
                      Fluorescence = c(dt$M1, dt$M2, dt$M3,
                                       pred))
   
@@ -59,17 +60,34 @@ plot.pred <- function(dt, pred) {
 # analyze one animal
 dt <- read.csv("GFR.csv", as.is=TRUE)
 animals <- unique(dt$Animal)
-a = animals[8]
-tmp <- subset(dt,Animal==a)
-tmp <- tmp[order(tmp$Time),]
-tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
-start <- which.max(tmp$mean)[1]
-tmp <- tmp[start:nrow(tmp),]
+results <- NULL
 
-fit <- NULL
-while (is.null(fit)) {
-  fit <- twoexp(y=tmp$mean,x=tmp$Time)
-  if (is.null(fit)) {} # filter outliers
+pdf("plot_10examples_GFR.pdf",width=12)
+plot(0,1)
+for (a in animals) {
+  tmp <- subset(dt,Animal==a)
+  tmp <- tmp[order(tmp$Time),]
+  
+  tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
+  start <- which.max(tmp$mean)[1]
+  tmp <- tmp[start:nrow(tmp),]
+  cf <- check.format(tmp)
+  if (cf!="") warning(cf)
+  fit1 <- oneexp(y=tmp$mean,x=tmp$Time)
+  fit2 <- twoexp(y=tmp$mean,x=tmp$Time)
+  
+  newrow <- data.frame(Animal = a, 
+                       GFR1 = ifelse(is.null(fit1), NA, coef(fit1)[2]/coef(fit1)[1]),
+                       GFR2 = ifelse(is.null(fit2), NA, coef(fit2)[2]*coef(fit2)[4]/(coef(fit2)[1]*coef(fit2)[4]+coef(fit2)[2]*coef(fit2)[3])))
+  results <- rbind(results, newrow)
+  
+  if (!is.null(fit1)) pred1 <- predict(fit1) else pred1 <- rep(mean(tmp$mean),nrow(tmp))
+  if (!is.null(fit2)) pred2 <- predict(fit2) else pred2 <- rep(mean(tmp$mean),nrow(tmp))
+  
+  p1 <- plot.pred(tmp, pred1) + ggtitle(paste(a,": one component model"))
+  p2 <- plot.pred(tmp, pred2) + ggtitle(paste(a,": two component model"))
+
+  require(cowplot)
+  plot(plot_grid(p1, p2))
 }
-
-plot.pred(tmp, predict(fit)) + ggtitle(paste(a,"two exp model"))
+dev.off()
