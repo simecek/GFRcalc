3eb302141231a883087e67262d191415f01c6c9b
simecek <lamparna@gmail.com>
2016-01-18 21:02:55 -0500

    added version and link to Github

--- ui.r
+++ ui.r
@@ -12,7 +12,10 @@ shinyUI(pageWithSidebar(
     sliderInput("trhold","Outlier threshold:", min=1, max=10, value=5),
     helpText("Input file should a XLSX file with 4-5 columns and no header:
               'Animal' (optional), 'Time', 'F1',
-              'F2' and 'F3' (other columns will be ignored).")
+              'F2' and 'F3' (other columns will be ignored)."),
+    br(),
+    div("GFRcalc 0.9.0, powered by R/Shiny, developed by Petr Simecek, source code on ",
+        a("Github", href="https://github.com/simecek/GFRcalc"))
     
   ),
   
